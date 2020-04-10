eba19a7c1926b90fe29cc422fbfc4334083daee9
joecof <joecofg@gmail.com>
2020-04-10 12:53:04 -0700

    added environment variables

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -57,10 +57,13 @@ class App extends Component {
     await fetch(`${CMS_BASE_URL}/api/socialmedia`)
         .then(res=> res.json())
         .then(result => {
+
           this.setState( {
-            socialMedia: result.socialMedia
+            socialMedia: result.socialMedias
           });
         });
+
+    
   };
   
   setTheme = () => {
